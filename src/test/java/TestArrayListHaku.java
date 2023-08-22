8378701af575cd5df1b18c4507d28fecaeeb91fe
Joni Kuutniemi <joni.kuutniemi@gmail.com>
2023-08-22 18:43:07 +0300

    Ohjelmoinnin työkalut 2023 t4.1

---
+++ src/test/java/TestArrayListHaku.java
@@ -0,0 +1,27 @@
+import fi.utu.tko2116.lista.OmaArrayList;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestArrayListHaku {
+
+    @Test
+    public void testaaListaSisältääLisätynArvon() {
+        int arvo = 9;
+        OmaArrayList oma = new OmaArrayList();
+        oma.lisaa(arvo);
+
+        assertEquals(true, oma.sisaltaako(arvo));
+    }
+
+    @Test
+    public void testaaOlematonArvonIndeksiOnMiinusYksi() {
+        int arvo = 9;
+        int eilöydy = arvo + 1;
+        OmaArrayList oma = new OmaArrayList();
+        oma.lisaa(arvo);
+
+        assertEquals(0, oma.indeksi(eilöydy));       
+    }
+
+}
